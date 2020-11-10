f51004e0f1dfcb572dd23bb5e280a1f6b558a9c3
Amarin Boonkirt <amarin@rvglobalsoft.com>
2020-11-10 20:38:18 +0700

    defind ipv4 for container

--- README-netway-devops.md
+++ README-netway-devops.md
@@ -1,4 +1,4 @@
-## Awesome People
+## First clone
 
 ~~~
 cd ~/gitworks
@@ -11,10 +11,9 @@ git clone -b develop --recurse-submodules https://github.com/netway-devops/rvsit
 cd develop
 git submodule foreach "git checkout develop"
 
-cd ~/gitworks/laradock-multisite
-./local-ssl-certs-gen.sh
-docker-compose up --build -d apache2 php-fpm mysql workspace phpmyadmin
-docker-compose -f docker-compose.yml exec --user laradock workspace bash
+open folder ~/gitworks/space/rvsitebuilder/develop
+
+open workspace file rvsitebuilder.code-workspace
 
 cd /var/www/rvsitebuildercms/rvsitebuilder.develop
 cp .env.dev .env
@@ -35,6 +34,10 @@ git submodule add -b develop  https://gitlab.rvglobalsoft.netway.pro/rvglobalsof
 
 
 Optional Docker Compose
+cd ~/gitworks/laradock-multisite
+docker-compose up --build -d traefik whoami apache2 php-fpm mysql workspace phpmyadmin
+docker-compose -f docker-compose.yml exec --user laradock workspace bash
+
 docker-compose -f docker-compose.yml logs -f apache2
 docker-compose down
 ~~~
---
+++ dashboard.php
@@ -0,0 +1,34 @@
+<?php
+echo '<h3>System Information</h3>';
+echo 'Current PHP version: ' . phpversion();
+echo '
+
+  <h3>Tools</h3>
+
+  <div>
+    phpMyAdmin
+    <a href="http://127.0.0.1:8081/" target="_blank">
+      http://127.0.0.1:8081/
+    </a>
+  </div>
+  <h3>RVsitebuilder Workspace</h3>
+  <h4>master</h4>
+  <pre>
+      RVsitebuilder CMS <a href="http://rvsitebuilder.master" target="_blank">http://rvsitebuilder.master</a>
+      RVsitebuilder CDN <a href="http://cdn.rvtheme.com.master/service/index.php?item=1&preview" target="_blank">http://cdn.rvtheme.com.master/service/index.php?item=1&preview</a>
+      RVglobalsoft Web <a href="https://rvglobalsoft.com.master" target="_blank">https://rvglobalsoft.com.master</a>
+      RVsitebuilder Developer Web <a href="https://dev.rvsitebuilder.com.master" target="_blank">https://dev.rvsitebuilder.com.master</a>
+      RVsitebuilder User Web <a href="https://user.rvsitebuilder.com.master" target="_blank">https://user.rvsitebuilder.com.master</a>
+      RVsitebuilder Apps Marketplace <a href="https://apps.rvsitebuilder.com.master" target="_blank">https://apps.rvsitebuilder.com.master</a>
+    </pre>
+  <h4>develop</h4>
+  <pre>
+      RVsitebuilder CMS <a href="http://rvsitebuilder.develop" target="_blank">http://rvsitebuilder.develop</a>
+      RVsitebuilder CDN <a href="http://cdn.rvtheme.com.develop/service/index.php?item=1&preview" target="_blank">http://cdn.rvtheme.com.develop/service/index.php?item=1&preview</a>
+      RVglobalsoft Web <a href="https://rvglobalsoft.com.master" target="_blank">https://rvglobalsoft.com.master</a>
+      RVsitebuilder Developer Web <a href="https://dev.rvsitebuilder.com.develop" target="_blank">https://dev.rvsitebuilder.com.develop</a>
+      RVsitebuilder User Web <a href="https://user.rvsitebuilder.com.develop" target="_blank">https://user.rvsitebuilder.com.develop</a>
+      RVsitebuilder Apps Marketplace Web <a href="https://apps.rvsitebuilder.com.develop" target="_blank">https://apps.rvsitebuilder.com.develop</a>
+    </pre>
+
+';
